e8319ba923e02fee1209a5801bc8e7a7a7e1d93c
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-10 20:03:36 -0700

    Finish key hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,9 +7,6 @@ class Enigma
   end
 
   def generate_key_hash
-    # split number to make an array to reduce over
-    @key.reduce({}) do |acc, number|
-      require "pry"; binding.pry
-    end
+    {a: @key[0..1].to_i, b: @key[1..2].to_i, c: @key[2..3].to_i, d: @key[3..4].to_i}
   end
 end
