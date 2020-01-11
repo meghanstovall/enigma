8dba2407436809d420e066be8cc31a2fb0295cc1
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-10 20:27:55 -0700

    Create shift hash

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -25,4 +25,8 @@ class EnigmaTest < Minitest::Test
   def test_it_can_create_key_hash
     assert_equal ({a: 1, b: 0, c: 2, d: 5}), @enigma.generate_offset_hash
   end
+
+  def test_can_create_hash_with_shift_number
+    assert_equal ({A: 3, B: 27, C: 73, D: 20}), @enigma.generate_shift_hash
+  end
 end
