132c0eb08a831298e244a9da8b38e9a96ea30b4b
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-11 12:12:26 -0700

    Finish testing the generation of the offset hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -19,8 +19,7 @@ class Enigma
     {a: key[0..1].to_i, b: key[1..2].to_i, c: key[2..3].to_i, d: key[3..4].to_i}
   end
 
-  def generate_offset_hash
-    offset = create_offset
+  def generate_offset_hash(offset = create_offset)
     {a: offset[0].to_i, b: offset[1].to_i, c: offset[2].to_i, d: offset[3].to_i}
   end
 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -36,10 +36,14 @@ class EnigmaTest < Minitest::Test
     assert_equal ({a: 02, b: 27, c: 71, d: 15}), @enigma.generate_key_hash("02715")
   end
 
-  # def test_it_can_create_offset_hash
-  #   assert_equal ({a: 1, b: 0, c: 2, d: 5}), @enigma.generate_offset_hash
-  # end
-  #
+  def test_it_can_create_offset_hash
+    assert_instance_of Hash, @enigma.generate_offset_hash
+    assert_equal 4, @enigma.generate_offset_hash.length
+    assert_equal [:a, :b, :c, :d], @enigma.generate_offset_hash.keys
+    assert_instance_of Integer, @enigma.generate_offset_hash.values[0]
+    assert_equal ({a: 1, b: 0, c: 2, d: 5}), @enigma.generate_offset_hash("1025")
+  end
+
   # def test_can_create_hash_with_shift_number
   #   assert_equal ({A: 3, B: 27, C: 73, D: 20}), @enigma.generate_shift_hash
   # end
