167279b9753a756c145518d96cd9176642ce29b1
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-10 20:14:03 -0700

    Create offset hash cooresponding to the letters

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,17 @@
 class Enigma
 
-  attr_reader :key
+  attr_reader :key, :offset
 
-  def initialize(key)
+  def initialize(key, offset)
     @key = key
+    @offset = offset
   end
 
   def generate_key_hash
     {a: @key[0..1].to_i, b: @key[1..2].to_i, c: @key[2..3].to_i, d: @key[3..4].to_i}
+  end
+
+  def generate_offset_hash
+    {a: @offset[0].to_i, b: @offset[1].to_i, c: @offset[2].to_i, d: @offset[3].to_i}
   end
 end
