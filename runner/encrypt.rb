35db2c634473a212bbffe7fcaebc945a7d6e50af
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-12 12:13:04 -0700

    Finish encrypt.rb runner file

--- runner/encrypt.rb
+++ runner/encrypt.rb
@@ -1,4 +1,19 @@
 require './lib/enigma'
 
 enigma = Enigma.new
-file = File.open
+
+# ARGV is an array that contains the values
+# passed by the command line
+file = File.open(ARGV[0], "r")
+message = file.read
+file.close
+
+encrypt = enigma.encrypt(message)
+
+# ARGV is an array that contains the values
+# passed by the command line
+writing = File.open(ARGV[1], "w")
+writing.write(encrypt[:encryption])
+writing.close
+
+puts "Created 'encrypted.txt' with the key #{encrypt[:key]} and date #{encrypt[:date]}"
